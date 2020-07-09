dbf8205b0025d37f39bd56805a3afb909edb7bc1
yuksalbek97 <yuksal97@gmail.com>
2020-07-09 10:49:04 -0400

    third commmit

--- src/index.js
+++ src/index.js
@@ -3,21 +3,23 @@ document.addEventListener('DOMContentLoaded', e => {
 
 
 const fetchBeers=()=> {
-    fetch('http://localhost:3000/beers/[:id]')
+    fetch('http://localhost:3000/beers/1')
     .then(resp => resp.json())
+    // .then(console.log)
     .then (beers=>{
-        beers.forEach(beer=> {
-            renderBeer(beer)
-        });
+     renderBeer(beers)
     })
 }
+
+fetchBeers()
 
 const renderBeer=beer=>{
     const beerDet = document.getElementsByClassName('beer-details')[0]
     const nameCard = document.querySelector('h2')
+    console.log(nameCard)
     const imageCard = document.querySelector('img')
      
-    nameCard.textContent = ${beer.name}
+    nameCard.textContent = beer.name
     beerDet.appendChild(nameCard)
 
 }
