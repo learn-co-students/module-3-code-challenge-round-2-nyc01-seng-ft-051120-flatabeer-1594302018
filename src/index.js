1838f59f042d3936fc35cb8de32010fff99a7ef3
chloekim917 <chloekim917@gmail.com>
2020-07-09 10:27:11 -0400

    second commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     getBeer()
+    changeDescription()
 })
 let beerDetail = document.querySelector('.beer-details')
-let reviewUl = document.querySelector('.reviews')
+// let reviewUl = document.querySelector('.reviews')
 
 function getBeer(){
     fetch("http://localhost:3000/beers/1")
@@ -18,7 +19,7 @@ function render(beer){
 
         <form class="description">
           <textarea>${beer.description}</textarea>
-          <button>Update Beer</button>
+          <button class='update-description'>Update Beer</button>
         </form>
 
         <h3>Leave a Review</h3>
@@ -31,10 +32,35 @@ function render(beer){
         <ul class="reviews">
         </ul>
     `
+    let reviewUl = document.querySelector('.reviews')
+
     beer.reviews.forEach(function(r){
         let reviewLi = document.createElement('li')
-        reviewLi.
+        reviewLi.innerText = `${r}`
+       reviewUl.append(reviewLi)
 
     })
 }
 
+function changeDescription(){
+    let descriptionForm = document.querySelector('.description')
+    let descriptionContent = descriptionForm.firstChild.nextElementSibling.textContent
+    // console.log(descriptionContent)
+    document.addEventListener('click', function(e){
+        if(e.target.className === 'update-description'){
+            e.preventDefault()
+            console.log(e.target)
+        }
+    })
+    // fetch("http://localhost:3000/beers/1", {
+    //     method: "PATCH",
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         'Accept': 'application/json'
+    //     },
+    //     body: JSON.stringify({
+    //         description: descriptionContent
+    //     })
+    // })
+    
+}
