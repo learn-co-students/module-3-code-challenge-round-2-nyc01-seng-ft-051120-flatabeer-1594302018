c91b20abb3eb37b2addac3ca7656f8b79e57aceb
github username <github email address>
2020-07-09 10:07:13 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 // Code here
+document.addEventListener("DOMContentLoaded", () => {
+
+    Base_URL = "http://localhost:3000/beers"
+fetchBeer()
+
+    function fetchBeer() {
+        fetch(`${Base_URL}/1`)
+        .then(resp => resp.json())
+        .then(beer => getBeer(beer))
+    }
+
+    function getBeer(beer){
+        document.querySelector(".beer-details")
+        h2 = document.querySelector("h2")
+        image = document.querySelector('img')
+        console.log(image)
+        h2.innerText = beer.name
+        image.src = beer.img_url
+
+        
+    }
+
+
+
+
+})
