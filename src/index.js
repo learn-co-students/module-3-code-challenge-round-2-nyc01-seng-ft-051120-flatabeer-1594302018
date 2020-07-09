7ba0dffd54eeb99602f090dbfc67a9c051fa79ab
yuksalbek97 <yuksal97@gmail.com>
2020-07-09 10:07:50 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 // Code here
+document.addEventListener('DOMContentLoaded', e => {
+
+
+const fetchBeers=()=> {
+    fetch('http://localhost:3000/beers/[:id]')
+    .then(resp => resp.json())
+    .then (beers=>{
+        beers.forEach(beer=> {
+            renderBeer(beer)
+        });
+    })
+}
+
+const renderBeer=beer=>{
+    const nameCard = document.querySelector('h2')
+    const imageCard = document.querySelector('img')
+
+}
+
+
+
+
+
+})
