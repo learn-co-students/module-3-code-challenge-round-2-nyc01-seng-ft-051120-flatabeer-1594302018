4f55c1843ef806711fe242df1e8d4c916b8dd819
github username <github email address>
2020-07-09 11:44:17 -0400

    deliverables

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     Base_URL = "http://localhost:3000/beers"
-fetchBeer()
-updateDescription()
-submitReview()
+    reviews = document.querySelector('.reviews')
+    
+    fetchBeer() 
+    updateDescription()
+    submitReview()
 
     function fetchBeer() {
         fetch(`${Base_URL}/1`)
@@ -17,7 +19,7 @@ submitReview()
         h2 = document.querySelector("h2")
         description = document.querySelector("#beer-desc")
         image = document.querySelector('img')
-        reviews = document.querySelector('.reviews')
+        reviews.innerHTML = ""
         beer.reviews.forEach(review => {
             let li = document.createElement('li')
             li.append(review)
@@ -26,8 +28,6 @@ submitReview()
         h2.innerText = beer.name
         image.src = beer.image_url
         description.innerText = beer.description
-        
-
     }
 
     function updateDescription(){
@@ -44,31 +44,19 @@ submitReview()
                     },
                     body: JSON.stringify({ description: descText.value })
                 })
-
             }
-            
-
-            
-
         })
     }
 
     function submitReview() {
         document.addEventListener("submit", e => {
             e.preventDefault()
-            // console.dir(e.target)
             if(e.target.className === "review-form"){
-                console.log = e.target[0].value
-
+                newReview = e.target[0].value
+                let li = document.createElement('li')
+                li.append(newReview)
+                reviews.append(li)
             }
-
         })
-
     }
-
-
-
-
-
-
 })
