c33474beef1d03d98f47dd38069eb9e979e9cae2
smatalha <sma.talha@gmail.com>
2020-07-09 11:20:53 -0400

    update deer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 // Code here
 
 document.addEventListener('DOMContentLoaded', () => {
+    let description = document.querySelector('.description')
 
-    fetch('http://localhost:3000/beers/2')
+    fetch('http://localhost:3000/beers/1')
     .then(resp=> resp.json())
     .then(beer => {
         renderBeer(beer)
@@ -14,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
         name.innerText = beer.name
         let image = beerDetails.childNodes[3]
         image.src = beer.image_url
-        let description = document.querySelector('.description')
+        // let description = document.querySelector('.description')
         let textArea = description.firstElementChild
         textArea.innerHTML = beer.description
         
@@ -24,18 +25,41 @@ document.addEventListener('DOMContentLoaded', () => {
         // comment.innerHTML = addComment(image.comments)
     }
 
-
     const renderReview = reviews => {
-        reviews.forEach(r => {
-        let ulReviews = document.querySelector('.reviews')
-        console.log(ulReviews);
-        
-        });
+        return reviews.map(r => {
+            return `<li>${r.reviews}</li>`
+        }).join("")
     }
-    //     return reviews.map(r => {
-    //         return `<li>${r.reviews}</li>`
-    //     }).join("")
-    // }
+
+    description.addEventListener('submit', e => {
+        e.preventDefault()
+        let form = e.target
+        let description = form[0].value
+        let beerObj = {
+            description: description
+        }
+        fetch('http://localhost:3000/beers/1', {
+            method: "PATCH",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(beerObj)
+        })
+    })
+
+    const addComment = () => {
+        let reviewForm = document.querySelector('.review-form')
+        reviewForm.addEventListener('submit', e => {
+            e.preventDefault()
+
+            let form = e.target
+            text = form[0].value
+            renderReview
+        })
+        
+    }
+    addComment()
 })
 
 
