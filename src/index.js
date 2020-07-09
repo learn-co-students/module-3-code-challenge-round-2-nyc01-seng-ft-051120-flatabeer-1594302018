103042d16340f43ff1d7827ea3118900efb6c37c
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2020-07-09 10:07:23 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-// Code here
+// // Code here
+// As a user, I can:
+
+// - See the first beer's details, including its **name, image, description, and reviews**, when the page loads
+// - Change the beer's description and **still see that change when reloading the page**
+// - Add a review for the beer (no persistence needed)
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    // console.log("dom loaded")
+    getBeers()
+
+});
+
+const URL = "http://localhost:3000/beers"
+
+function getBeers(){
+    fetch(URL)
+    .then(resp => resp.json())
+    // .then(console.log)
+    .then(beers =. {
+        beers.forEach(beer => {
+            renderBeers(beer)
+        })
+    })
+}
+function renderBeers(beer){
+    let beerDiv = document.querySelector("beer-details")
+    beerDiv.innerHTML =`
+    `
+    
+}
