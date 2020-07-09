93c1787988c1ef9612a74781b284d84441ab0e3e
Ideal-Poster <malcolmgourdine@protonmail.com>
2020-07-09 10:07:20 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 // Code here
+BASE_URL="http://localhost:3000"
+BEERS_URL=`${BASE_URL}/beers`
+
+const beerDetails = document.querySelector('.beer-details');
+const beerName = document.querySelector('h2');
+const beerImage = beerDetails.querySelector('img');
+const beerDescriptionForm = beerDetails.querySelector('.description');
+const beerReviewForm = beerDetails.querySelector('.review-form');
+const beerReviewsList = beerDetails.querySelector('.reviews');
+
+
+
+getBeer = (id, callback) => {  
+  let requestOptions = {
+    method: 'GET',
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+  
+  fetch(`${BEERS_URL}/${id}`, requestOptions)
+    .then(response => response.json())
+    .then(result => callback(result))
+    .catch(error => console.log('error', error));
+}
+
+renderBeerDetails = json => {
+  beerName.innerText = json.name;
+  beerImage.src = json.image_url;
+  const beerReviews = Array.from(beerReviewsList.children);
+  beerReviews.map(review => {
+    console.log(review);
+    
+  })
+}
+
+
+getBeer(1, renderBeerDetails)
