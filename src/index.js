49a6abb3e89d6209bbe4b23076f643c512c7a222
JulianCedric <julian.pormentilla@gmail.com>
2020-07-09 10:59:46 -0400

    change beer description

--- src/index.js
+++ src/index.js
@@ -28,10 +28,40 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     fetchBeers("http://localhost:3000/beers")
 
-/////// 
+/////// PATCH - fetch to "http://localhost:3000/beers/beers/[:id]" with updated beer description
 
 // - Change the beer's description and **still see that change when reloading the page**
+    // ** will need to send a patch request to update this specific beer object in the database (to persist data /changes)
 
+    function patchBeerDescription(url, updatedDescription){
+        fetch(url), {
+            method: "PATCH", 
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }, 
+            body: JSON.stringify({
+            // here, we need to turn the JS value (of the description key / within the beer object) into a JSON string
+                description: updatedDescription
+            })
+        }
+        console.log("test3")
+    }
+
+// we need to add an event listener to the form and conditional around the button (update beer)
+
+    const descriptionForm = document.getElementsByClassName("description")
+
+    descriptionForm.addEventListener("click", function(e) => {
+        
+
+
+    })
+
+
+    // patchBeerDescription()
+
+    // function updateBeerDescription()
 
 
 })
